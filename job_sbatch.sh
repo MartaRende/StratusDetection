c18747b0caeac4a714e1deb6d97cfd2bf77e3fd5
MartaRende <martarende03@gmail.com>
2025-05-27 13:02:30 +0200

    wip

--- job_sbatch.sh
+++ job_sbatch.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=train_model    # create a short name for your job
 #SBATCH --nodes=1                # node count
 #SBATCH --ntasks=1               # total number of tasks across all nodes
-#SBATCH --cpus-per-task=4        # cpu-cores per task (>1 if multi-threaded tasks)
-#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G per cpu-core is default)
+#SBATCH --cpus-per-task=16        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=16G         # memory per cpu-core (4G per cpu-core is default)
 #SBATCH --time=00:30:00          # total run time limit (HH:MM:SS)
 #SBATCH --gres=gpu:1             # number of gpus per node
 #SBATCH --output=logs/%x_%j.out      # standard output
