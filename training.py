1d1b121c2267688b389c599111d9904132c5800b
MartaRende <martarende03@gmail.com>
2025-05-27 12:44:55 +0200

    wip

--- training.py
+++ training.py
@@ -116,13 +116,13 @@ accuracies = {
     "test": []
 }
 
-patience = 3  
+patience = 10 
 best_val_loss = float('inf')
 epochs_no_improve = 0
 
 print("len(data_test):", len(prepare_data.test_data))
 
-num_epochs = 7
+num_epochs = 50
 for epoch in range(num_epochs):
     print(f"Epoch {epoch+1}/{num_epochs}")
     for step in ["train", "eval", "test"]:
