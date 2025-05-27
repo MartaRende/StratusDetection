f7020665dca47bdcc79b47e83ea0b24e7013fe7a
MartaRende <martarende03@gmail.com>
2025-05-27 15:54:01 +0200

    wip

---
+++ inference_sbatch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --job-name=train_model    # create a short name for your job
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=1               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=16        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=16G         # memory per cpu-core (4G per cpu-core is default)
+#SBATCH --time=00:30:00          # total run time limit (HH:MM:SS)
+#SBATCH --gres=gpu:1             # number of gpus per node
+#SBATCH --output=logs/%x_%j.out      # standard output
+#SBATCH --error=logs/%x_%j.err       # standard error
+#SBATCH --nodelist=chacha            # nodes to use
+#module purge
+apptainer exec --nv --bind /data/datasets/photocast:/data/datasets/photocast ./train.sif python3 -u inference.py
