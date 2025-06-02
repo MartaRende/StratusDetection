ec706a7cdd3c7944c0195f2aace3198c79ab5cf1
MartaRende <martarende03@gmail.com>
2025-06-02 11:02:06 +0200

    sbatch fixed

--- train_sbatch.sh
+++ train_sbatch.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=16        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=16G         # memory per cpu-core (4G per cpu-core is default)
-#SBATCH --time=00:30:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=01:30:00          # total run time limit (HH:MM:SS)
 #SBATCH --gres=gpu:1             # number of gpus per node
 #SBATCH --output=logs/%x_%j.out      # standard output
 #SBATCH --error=logs/%x_%j.err       # standard error
