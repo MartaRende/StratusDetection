19718c3d1865520b99d784bb5930c92ae9af6c0f
MartaRende <martarende03@gmail.com>
2025-06-02 13:43:49 +0200

    test new model

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 # Path: model.py
 # Path: model.py
+# Path: model.py
 import torch
 import torch.nn as nn
 
@@ -12,23 +13,23 @@ class StratusModel(nn.Module):
             nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2), # 512 x512 --> 256 x256
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),   # 256 x 256 --> 128 x 128
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),   # 128 x 128 --> 64 x 64
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),   # 64 x 64 --> 32 x 32
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),   # 32 x 32 --> 16 x 16
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(2, 2),   # 16 x 16 --> 8 x 8  
+            # nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1),
+            # nn.ReLU(),
+            # nn.MaxPool2d(2, 2),   # 16 x 16 --> 8 x 8  
         )
-        self.cnn_output_size = 32 * 8 * 8  # 32 channels * 8x8 spatial = 2048
+        self.cnn_output_size = 32 * 16 * 16  # 32 channels * 8x8 spatial = 2048
         
         # MLP for meteorological data
         self.mlp_meteo = nn.Sequential(
@@ -38,12 +39,20 @@ class StratusModel(nn.Module):
             nn.Linear(64, 64),
             nn.ReLU(),
             nn.Dropout(0.5),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+
+
 
         )
         
         # Final classification head
         self.mlp_head = nn.Sequential(
-            nn.Linear(self.cnn_output_size + 64, 128),
+            nn.Linear(self.cnn_output_size + 32, 128),
             nn.ReLU(),
             nn.Dropout(0.5),
             nn.Linear(128, 64),
