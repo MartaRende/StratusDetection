b3b045c8a46a458524a85f97526d187f75c94676
MartaRende <martarende03@gmail.com>
2025-06-03 19:20:07 +0200

    model size reduced 2

--- model.py
+++ model.py
@@ -9,25 +9,21 @@ class StratusModel(nn.Module):
         super(StratusModel, self).__init__()
         
         # CNN for image processing
+     # CNN for image processing
         self.cnn = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2), # 512 x512 --> 256 x256
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),   # 256 x 256 --> 128 x 128
             nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),   # 128 x 128 --> 64 x 64
             nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),   # 64 x 64 --> 32 x 32
             nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),   # 32 x 32 --> 16 x 16
             # nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1),
@@ -40,34 +36,27 @@ class StratusModel(nn.Module):
         self.mlp_meteo = nn.Sequential(
             nn.Linear(input_data_size, 64),
             nn.ReLU(),
-            nn.Dropout(0.5),
+            nn.Dropout(0.3),
             nn.Linear(64, 64),
             nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(64, 64),
-            nn.ReLU(),
-            nn.Dropout(0.5),
-
-
+            nn.Dropout(0.3),
 
         )
         
         # Final classification head
         self.mlp_head = nn.Sequential(
-            nn.Linear(self.cnn_output_size * 2 + 64, 128),
+            nn.Linear(self.cnn_output_size + 64, 128),
             nn.ReLU(),
-            nn.Dropout(0.5),
+            nn.Dropout(0.3),
             nn.Linear(128, 64),
             nn.ReLU(),
-            nn.Dropout(0.5),
+            nn.Dropout(0.3),
             nn.Linear(64, 32),
             nn.ReLU(),
-            nn.Dropout(0.5),
+            nn.Dropout(0.3),
             nn.Linear(32, output_size) 
         )
+
 
     def forward(self, weather_x, image1_x, image2_x):
         x1 = self.cnn(image1_x)
