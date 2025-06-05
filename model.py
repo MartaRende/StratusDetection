d7056c39f38784aba4a56a92d4e6501216bfc939
MartaRende <martarende03@gmail.com>
2025-06-05 10:20:24 +0200

    batch norm added

--- model.py
+++ model.py
@@ -11,18 +11,23 @@ class StratusModel(nn.Module):
         # CNN for image processing
         self.cnn = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2, 2), # 512 x512 --> 256 x256
             nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),   # 256 x 256 --> 128 x 128
             nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),   # 128 x 128 --> 64 x 64
             nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),   # 64 x 64 --> 32 x 32
             nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),   # 32 x 32 --> 16 x 16
             # nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1),
