bbe47aa6e02f06bbed7edfd4d63935f3be30e768
MartaRende <martarende03@gmail.com>
2025-06-11 07:47:41 +0200

    wip

--- inference_sbatch.sh
+++ inference_sbatch.sh
@@ -4,10 +4,9 @@
 #SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=4        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G per cpu-core is default)
-#SBATCH --time=01:30:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=23:30:00          # total run time limit (HH:MM:SS)
 #SBATCH --gres=gpu:1             # number of gpus per node
 #SBATCH --gres=shard:24          # number of gpu shards to use
-
 #SBATCH --output=logs/%x_%j.out      # standard output
 #SBATCH --error=logs/%x_%j.err       # standard error
 #SBATCH --nodelist=chacha            # nodes to use
