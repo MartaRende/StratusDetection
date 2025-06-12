dee2e5cddaed4befaf45e6cae4f07498e8c763e5
MartaRende <martarende03@gmail.com>
2025-06-12 08:07:43 +0200

    wip

--- train_sbatch.sh
+++ train_sbatch.sh
@@ -11,4 +11,4 @@
 #SBATCH --error=logs/%x_%j.err       # standard error
 #SBATCH --nodelist=chacha            # nodes to use
 #module purge
-apptainer exec --nv --bind /data/datasets/photocast:/data/datasets/photocast /data/datasets/marta.rende/train.sif python3 -u inference.py 1 1
+apptainer exec --nv --bind /data/datasets/photocast:/data/datasets/photocast /data/datasets/marta.rende/train.sif python3 -u training.py 1 1
