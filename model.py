d732a5d06d0755f3630619f301e1a8191cc27327
MartaRende <martarende03@gmail.com>
2025-06-15 21:18:14 +0200

    wip

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 # Path: model.py
 # Path: model.py
 # Path: model.py
+# Path: model.py
 import torch
 import torch.nn as nn
 
@@ -19,19 +20,19 @@ class StratusModel(nn.Module):
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),  # 256x256 -> 128x128
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),  # 128x128 -> 64x64
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),  # 64x64 -> 32x32
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),  # 32x32 -> 16x16
         )
-
-
-        self.cnn_output_size = 64 * 16 * 16  # 16384
+        
+        
+        self.cnn_output_size = 128 * 16 * 16  # 8192 
 
         # MLP for weather data
         self.mlp_meteo = nn.Sequential(
@@ -49,20 +50,30 @@ class StratusModel(nn.Module):
 
         # MLP final
         self.mlp_head = nn.Sequential(
-            nn.Linear(mlp_input_size, 256),  # 512
+            nn.Linear(mlp_input_size, 4096),
             nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(256, 256),
+            nn.Dropout(0.3),
+            nn.Linear(4096, 4096),
             nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(256, 128),
+            nn.Dropout(0.3),
+            nn.Linear(4096, 2048),
             nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(128, 128),
+            nn.Dropout(0.3),
+            nn.Linear(2048, 2048),
             nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(128, output_size)
+            nn.Dropout(0.3),
+            nn.Linear(2048, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(1024, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(1024, 1024),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(1024, output_size)
         )
+
 
     def forward(self, meteo_seq, image_seq_1, image_seq_2=None):
         batch_size = image_seq_1.size(0)
