1faf0ff0ee15f9bb986bdc7e2dcf32b5386ecf40
MartaRende <martarende03@gmail.com>
2025-06-17 10:31:24 +0200

    test train with approach to not full ram

--- model.py
+++ model.py
@@ -50,16 +50,7 @@ class StratusModel(nn.Module):
 
         # MLP final
         self.mlp_head = nn.Sequential(
-            nn.Linear(mlp_input_size, 2048),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(2048, 2048),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(2048, 2048),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(2048, 1024),
+            nn.Linear(mlp_input_size, 1024),
             nn.ReLU(),
             nn.Dropout(0.3),
             nn.Linear(1024, 1024),
@@ -68,10 +59,13 @@ class StratusModel(nn.Module):
             nn.Linear(1024, 512),
             nn.ReLU(),
             nn.Dropout(0.3),
-            nn.Linear(512, 256),
+            nn.Linear(512, 512),
             nn.ReLU(),
             nn.Dropout(0.3),
-            nn.Linear(256, output_size)
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(128, output_size)
         )
 
 
