19727a639306fa05f0fd3f9548f8b69e0570c3ae
MartaRende <martarende03@gmail.com>
2025-06-18 07:58:29 +0200

    radiotion deleted fixed

--- training.py
+++ training.py
@@ -124,7 +124,7 @@ train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=16, shuffle
 validation_loader = torch.utils.data.DataLoader(validation_dataset, batch_size=16)
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=16)
 # Instantiate model, loss, optimizer, scheduler
-model = StratusModel(input_feature_size=15, output_size=2, num_views=num_views, seq_len=seq_len).to(device)
+model = StratusModel(input_feature_size=13, output_size=2, num_views=num_views, seq_len=seq_len).to(device)
 loss_fn = torch.nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.1, patience=5)
