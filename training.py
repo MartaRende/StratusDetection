696fe84f757c4369272dc84876c31bc5390d6330
MartaRende <martarende03@gmail.com>
2025-06-20 19:17:38 +0200

    train small dataset

--- training.py
+++ training.py
@@ -40,7 +40,7 @@ FP_WEATHER_DATA = "data/complete_data.npz"
 prepare_data = PrepareData(FP_IMAGES, FP_WEATHER_DATA, num_views=num_views,seq_length=seq_len)
 
 # Load filtered data
-x_meteo, x_images, y = prepare_data.load_data(end_date="2023-01-07")
+x_meteo, x_images, y = prepare_data.load_data()
 print("Data after filter:", x_meteo.shape, x_images.shape, y.shape)
 
 # Concatenate all data if multiple sources (your code suggests potential multiple)
