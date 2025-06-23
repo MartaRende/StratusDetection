235dc60a8af36b3e9bcf06fac4e3021d8b01a935
MartaRende <martarende03@gmail.com>
2025-06-23 12:34:13 +0200

    train without rads

--- model.py
+++ model.py
@@ -22,16 +22,16 @@ class StratusModel(nn.Module):
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),  # 128x128 -> 64x64
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),  # 64x64 -> 32x32
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),  # 32x32 -> 16x16
         )
 
 
-        self.cnn_output_size = 128 * 16 * 16  # 8192 features after CNN layers
+        self.cnn_output_size = 64 * 16 * 16  # 8192 features after CNN layers
 
         # MLP for weather data
         self.mlp_meteo = nn.Sequential(
