5b5415e9c0924bad3a506674efacceb8a8645ee2
MartaRende <martarende03@gmail.com>
2025-06-30 20:07:07 +0200

    wip

--- inference_multiple.py
+++ inference_multiple.py
@@ -67,7 +67,8 @@ non_stratus_days = []
 all_predicted = {t: [] for t in prediction_times}
 all_expected = {t: [] for t in prediction_times}
 
-months = [(2023, m) for m in range(1, 4)] + [(2023, m) for m in range(9, 13)] + [(2024, m) for m in range(1, 4)] + [(2024, m) for m in range(9, 13)]
+months = [(2024, m) for m in range(12, 13)]
+#+ [(2023, m) for m in range(9, 13)] + [(2024, m) for m in range(1, 4)] + [(2024, m) for m in range(9, 13)]
 
 for year, month in months:
     start_date = f"{year}-{month:02d}-01"
